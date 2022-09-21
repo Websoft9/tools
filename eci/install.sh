9aa85a410afae1b3d1ead0c237250882f7f3b8b0
Darren <27513732@qq.com>
2022-09-21 17:43:04 +0800

    Create install.sh

---
+++ eci/install.sh
@@ -0,0 +1,10 @@
+# install ECI cli
+sudo bash -c "$(curl -s https://eci-release.oss-cn-beijing.aliyuncs.com/install.sh)"
+
+
+eci config set-context \
+--access-key-id LTAIyHnO9KCe4awI \
+--access-secret ×××××××××××××××××××××××××××××××× \
+--region-id cn-zhangjiakou \
+--security-group-id sg-8vbe54utt8gjfy6irp9p \
+--v-switch-id vsw-8vbdt4t3nvfmnmjwl3miv
